55c835ba32250abe54ad2c8028397f705a467700
Udemba Godswill <godswill.udemna.pg88009@unn.edu.ng>
2024-01-30 22:55:05 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const addToCount = () => {
+  count.value = count.value + 1;
+};
+
+const minusFromCount = () => {
+  count.value = count.value - 1;
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>Our Count is ...</h4>
+      <h1>
+        {{ count }}
+      </h1>
+      <button @click="minusFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: rgb(223, 230, 233);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
